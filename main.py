857d7d5842fb5579e5ec8795f30bafb976e02285
giulianotattico <162876524+giulianotattico@users.noreply.github.com>
2024-04-07 11:53:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-from settings import settings
 import discord
-# import * - è un modo rapido per importare tutti i file della libreria.
-from bot_logic import *
 
-# La variabile intents memorizza i privilegi del bot
+
+# la variabile intents contiene i permessi al bot
+
 intents = discord.Intents.default()
-# Abilitazione del privilegio di lettura dei messaggi
-intents.message_content = True
-# Creare un bot nella variabile client e trasferirgli i privilegi
+
+
+
+# abilita il permesso a leggere i contenuti dei messaggi
+discord.Intents.message_content = True
+
+
+# crea un bot e passa gli indents
+
 client = discord.Client(intents=intents)
 
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+        print(f"Abbiamo fatto l'accesso come {client.user}")
 
-# Una volta che il bot è pronto, stamperà il suo nome!
-@client.event
-async def on_ready():
-    print(f'Abbiamo effettuato l'accesso come {client.user}')
+    async def on_message(self, message):
+        if message.content.startswith('!deleteme'):
+            msg = await message.channel.send('I will delete myself now...')
+            await msg.delete()
+
+            # this also works
+            await message.channel.send('Goodbye in 3 seconds...', delete_after=3.0)
+
+    async def on_message_delete(self, message):
+        msg = f'{message.author} has deleted the message: {message.content}'
+        await message.channel.send(msg)
 
 
-# Quando il bot riceve un messaggio, invia messaggi nello stesso canale!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Ciao! Sono un bot!')
-    elif message.content.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    else:
-        await message.channel.send("Non è possibile elaborare questo comando, mi dispiace!")
 
-client.run(settings["TOKEN"])
+
+client = MyClient(intents=intents)
+client.run('MTIyMTM5OTQwNzE1NzU3NTc2MQ.GthGV4.9cyVdNXs-LD3i761rnDc7MZt_OXcubx4gd0jK4')
